45c2048310e22b0da297aab23f8058ce7b1004b4
Tessla Muir <tesslamuir@yahoo.com>
2022-10-26 01:59:16 -0400

    Particle effects

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     public float respawnTime = 1.2f;
-    
+    public ParticleSystem deathEffect;
+
     void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.tag == "Ground")
         {
+            deathEffect.Play();
             Invoke("death", respawnTime);
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     public float finishTime = 1.2f;
+    public ParticleSystem finishEffect;
 
     void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.tag == "Player") {
+            finishEffect.Play();
             Invoke("finish", finishTime);
         }
     }
